58ed5f7bea02dbc86e8110a1b8e8533fbf998df5
Sudeep <sudeeptakumarpanda@gmail.com>
2023-10-13 18:44:36 +0530

    user  13.10.2023

--- Src/screen/Tabs/user/Home.js
+++ Src/screen/Tabs/user/Home.js
@@ -1,65 +1,127 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, } from 'react-native'
-import React from 'react'
-//import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import Main from './Main';
+import Search from './Search';
+import Wishlist from './Wishlist';
+import Orders from './Orders';
+import Profile from './Profile';
 
 const Home = () => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.bottomTabView}>
-                <TouchableOpacity style={styles.bottomTab}>
-                    <Image 
-                    source={require('../images/edit.png')} 
-                    style={styles.bottomIcon} />
-
-
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.bottomTab}>
-                    <Image source=
-                    {require('../images/edit.png')} 
-                    style={styles.bottomIcon} />
-
-
-                </TouchableOpacity>
-
-
-            </View>
-        </View>
-    )
-}
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <View style={styles.container}>
+      {selectedTab == 0 ? (
+        <Main />
+      ) : selectedTab == 1 ? (
+        <Search />
+      ) : selectedTab == 2 ? (
+        <Wishlist />
+      ) : selectedTab == 3 ? (
+        <Orders />
+      ) : (
+        <Profile />
+      )}
+      <View style={styles.bottomTabView}>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(0);
+          }}>
+          <Image
+            source={
+              selectedTab == 0
+                ? require('../images/home_fill.png')
+                : require('../images/home.png')
+            }
+            style={styles.bottomIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(1);
+          }}>
+          <Image
+            source={
+              selectedTab == 1
+                ? require('../images/search.png')
+                : require('../images/loupe.png')
+            }
+            style={styles.bottomIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(2);
+          }}>
+          <Image
+            source={
+              selectedTab == 2
+                ? require('../images/shop.png')
+                : require('../images/wishlist.png')
+            }
+            style={styles.bottomIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(3);
+          }}>
+          <Image
+            source={
+              selectedTab == 3
+                ? require('../images/cart.png')
+                : require('../images/checkout.png')
+            }
+            style={styles.bottomIcon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(4);
+          }}>
+          <Image
+            source={
+              selectedTab == 4
+                ? require('../images/profilefill.png')
+                : require('../images/user.png')
+            }
+            style={styles.bottomIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 export default Home;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
     bottomTabView: {
         flexDirection: 'row',
-        height: 60,
+        height: 50,
         width: '100%',
         backgroundColor: '#fff',
         justifyContent: 'space-evenly',
         alignItems: 'center',
         elevation: 5,
         position: 'absolute',
-        bottom: 0,
+        bottom: 5,
     },
     bottomTab: {
-        width: '20%',
+        width: '30%',
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-
-
     },
     bottomIcon: {
-        width: '24%',
-        height: '24%',
-        // justifyContent: 'center',
-        //alignItems:'center',
-
-
-
+        width: '20%',
+        height: '40%',
     },
-
 });
